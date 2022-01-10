59ad14fb95a9b4e154d9b1fbf2d071bb0b7d4702
Gustavo Martínez <gustavomartinez1990@gmail.com>
2022-01-10 11:45:22 -0400

    feat(express): add route for products and json response

--- index.js
+++ index.js
@@ -7,6 +7,24 @@ app.get('/', (req, res) => {
   res.send('Hello this is my app');
 });
 
+app.get('/new-route', (req, res) => {
+  res.send('Hello this is a new route for my app');
+});
+
+app.get('/products', (req, res) => {
+  res.json([
+    {
+      name: 'Product1',
+      price: 1000
+    },
+    {
+      name: 'Product2',
+      price: 1400
+    },
+  ]);
+});
+
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
