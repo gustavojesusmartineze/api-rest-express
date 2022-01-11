d8361d406cf00a7d0f2225bc859220efd14211d6
Gustavo Martínez <gustavomartinez1990@gmail.com>
2022-01-11 18:45:59 -0400

    feat(express): add route for products and categories with parameters

--- index.js
+++ index.js
@@ -14,16 +14,44 @@ app.get('/new-route', (req, res) => {
 app.get('/products', (req, res) => {
   res.json([
     {
-      name: 'Product1',
+      name: 'Product0',
       price: 1000
     },
     {
-      name: 'Product2',
+      name: 'Product1',
       price: 1400
     },
+    {
+      name: 'Product2',
+      price: 2400
+    },
+    {
+      name: 'Product3',
+      price: 5000
+    }
   ]);
 });
 
+app.get('/products/:productId', (req, res) => {
+  // const id = req.params.productId;
+  // This is an example using object destructuring
+  const { productId } = req.params;
+  res.json(
+    {
+      productId,
+      name: 'Product3',
+      price: 5000
+    }
+  );
+});
+
+app.get('/categories/:categoryId/products/:productId', (req, res) => {
+  const { categoryId, productId } = req.params;
+  res.json({
+    categoryId,
+    productId,
+  });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
