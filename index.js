27d8caadf8d9b3e93d9ad3318d0d00e9d0d60bad
Gustavo Martínez <gustavomartinez1990@gmail.com>
2022-01-12 17:51:44 -0400

    feat(products): use products routes on main app

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { faker } = require('@faker-js/faker');
+const routerApi = require('./routes/index');
 
 const app = express();
 const port = 3000;
@@ -8,42 +8,10 @@ app.get('/', (req, res) => {
   res.send('Hello this is my app');
 });
 
+routerApi(app);
+
 app.get('/new-route', (req, res) => {
   res.send('Hello this is a new route for my app');
-});
-
-app.get('/products', (req, res) => {
-  const { size } = req.query;
-  const limit = size || 10;
-
-  const products = [];
-  for (let i = 0; i < limit; i++) {
-    products.push({
-      name: faker.commerce.productName(),
-      description: faker.commerce.productDescription(),
-      price: parseInt(faker.commerce.price(), 10),
-      image: faker.image.imageUrl(),
-    })
-  }
-
-  res.json(products);
-});
-
-app.get('/products/filter', (req, res) => {
-  res.send('Im a filter');
-});
-
-app.get('/products/:productId', (req, res) => {
-  // const id = req.params.productId;
-  // This is an example using object destructuring
-  const { productId } = req.params;
-  res.json(
-    {
-      productId,
-      name: 'Product3',
-      price: 5000
-    }
-  );
 });
 
 app.get('/categories/:categoryId/products/:productId', (req, res) => {
