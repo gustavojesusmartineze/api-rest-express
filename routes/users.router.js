283b69461282b36056781334c4bca0dd7deec351
Gustavo Martínez <gustavomartinez1990@gmail.com>
2022-01-14 19:58:33 -0400

    feat(users): add fake data for get route

--- routes/users.router.js
+++ routes/users.router.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const { faker } = require('@faker-js/faker');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/filtered', (req, res) => {
   const { limit, offset } = req.query;
 
   if (limit && offset) {
@@ -13,6 +14,20 @@ router.get('/', (req, res) => {
   } else {
     res.send('There are no query parameters');
   }
+});
+
+router.get('/', (req, res) => {
+  const users = [];
+  const { size } = req.query;
+  const limit = size || 10;
+  for (let i = 0; i < limit; i++) {
+    users.push({
+      name: faker.name.firstName(),
+      lastName: faker.name.lastName(),
+      image: faker.image.imageUrl(),
+    });
+  }
+  res.json(users);
 });
 
 module.exports = router;
