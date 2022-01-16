4edd1af3d864e4f3ca949d77181d8f552ccce03c
Gustavo Martínez <gustavomartinez1990@gmail.com>
2022-01-16 14:03:13 -0400

    feat(products): use product service in product router

--- routes/products.router.js
+++ routes/products.router.js
@@ -1,21 +1,11 @@
 const express = require('express');
-const { faker } = require('@faker-js/faker');
+const ProductsService = require('./../services/product.service');
 
 const router = express.Router();
+const service = new ProductsService();
 
 router.get('/', (req, res) => {
-  const { size } = req.query;
-  const limit = size || 10;
-
-  const products = [];
-  for (let i = 0; i < limit; i++) {
-    products.push({
-      name: faker.commerce.productName(),
-      description: faker.commerce.productDescription(),
-      price: parseInt(faker.commerce.price(), 10),
-      image: faker.image.imageUrl(),
-    })
-  }
+  const products = service.find();
 
   res.json(products);
 });
@@ -28,28 +18,18 @@ router.get('/:id', (req, res) => {
   // const id = req.params.id;
   // This is an example using object destructuring
   const { id } = req.params;
+  const product = service.findOne(id);
 
-  if (id === '999') {
-    res.status(404).json({
-      message: 'Not found'
-    });
-  }
-
-  res.status(200).json(
-    {
-      id,
-      name: 'Product3',
-      price: 5000
-    }
-  );
+  res.status(200).json(product);
 });
 
 router.post('/', (req, res) => {
   const body = req.body;
+  const product = service.create(body);
 
   res.status(201).json({
     message: 'created',
-    data: body
+    data: product
   })
 });
 
@@ -57,31 +37,26 @@ router.put('/:id', (req, res) => {
   const { id } = req.params;
   const body = req.body;
 
-  res.json({
-    message: 'updated',
-    data: body,
-    id
-  })
+  const product = service.update(id, body);
+
+  res.json(product)
 });
 
 router.patch('/:id', (req, res) => {
   const { id } = req.params;
   const body = req.body;
 
-  res.json({
-    message: 'patched',
-    data: body,
-    id
-  })
+  const product = service.update(id, body);
+
+  res.json(product)
 });
 
 router.delete('/:id', (req, res) => {
   const { id } = req.params;
 
-  res.status(204).json({
-    message: 'deleted',
-    id
-  })
+  const result = service.delete(id);
+
+  res.json(result);
 });
 
 module.exports = router;
