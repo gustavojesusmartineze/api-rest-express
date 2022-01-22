70c45f2800d9a0656266e29feb30b5d29176cb40
Gustavo Martínez <gustavomartinez1990@gmail.com>
2022-01-22 13:38:34 -0400

    chore: add config of allowed domains for cors

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const express = require('express');
+const cors = require('cors');
 const routerApi = require('./routes/index');
 const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler')
 
 const app = express();
 const port = 3000;
 
+const whitelist = ['http://localhost:5500', 'http://localhost:3500'];
+
+const options = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Forbidden'))
+    }
+  }
+};
+
 app.use(express.json());
+app.use(cors(options));
 
 app.get('/', (req, res) => {
   res.send('Hello this is my app');
