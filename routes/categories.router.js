7eeedc413002180acdb6690299c5c9ed22fe6f01
Gustavo Martínez <gustavomartinez1990@gmail.com>
2022-02-14 17:45:14 -0400

    feat(category): add routes for get and show

--- routes/categories.router.js
+++ routes/categories.router.js
@@ -1,13 +1,34 @@
 const express = require('express');
 
-const router = express.Router();
+const CategoryService = require('./../services/category.service');
+const validatorHandler = require('./../middlewares/validator.handler');
+const { createCategorySchema, updateCategorySchema, getCategorySchema } = require('./../schemas/category.schema');
 
-router.get('/:categoryId/products/:productId', (req, res) => {
-  const { categoryId, productId } = req.params;
-  res.json({
-    categoryId,
-    productId
-  });
+const router = express.Router();
+const service = new CategoryService();
+
+router.get('/', async (req, res, next) => {
+  try {
+    const categories = await service.find();
+    res.json(categories);
+  } catch (error) {
+    next(error);
+  }
 });
+
+router.get('/:id',
+  validatorHandler(getCategorySchema, 'params'),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const category = await service.findOne(id);
+      res.json(category);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+
 
 module.exports = router;
