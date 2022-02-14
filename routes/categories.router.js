658e16f7fcaa6504dcf56d2360f8a73a462f76e1
Gustavo Martínez <gustavomartinez1990@gmail.com>
2022-02-14 17:59:08 -0400

    feat(category): add routes for put and delete

--- routes/categories.router.js
+++ routes/categories.router.js
@@ -57,4 +57,32 @@ router.patch('/:id',
   }
 );
 
+router.put('/:id',
+  validatorHandler(getCategorySchema, 'params'),
+  validatorHandler(updateCategorySchema, 'body'),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const body = req.body;
+      const category = await service.update(id, body);
+      res.json(category);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+router.delete('/:id',
+  validatorHandler(getCategorySchema, 'params'),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      await service.delete(id);
+      res.status(201).json({id});
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
 module.exports = router;
