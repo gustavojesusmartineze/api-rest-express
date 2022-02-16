65a783c6d99e49c0b3b56886aa063aace9c74c42
Gustavo Martínez <gustavomartinez1990@gmail.com>
2022-02-15 20:48:21 -0400

    refactor(product): service

--- services/product.service.js
+++ services/product.service.js
@@ -10,16 +10,15 @@ class ProductsService {
 
   generate() {
     const limit = 100;
-
-    for (let i = 0; i < limit; i++) {
+    for (let index = 0; index < limit; index++) {
       this.products.push({
         id: faker.datatype.uuid(),
         name: faker.commerce.productName(),
         description: faker.commerce.productDescription(),
         price: parseInt(faker.commerce.price(), 10),
         image: faker.image.imageUrl(),
-        isBlocked: faker.datatype.boolean()
-      })
+        isBlock: faker.datatype.boolean(),
+      });
     }
   }
 
@@ -28,27 +27,21 @@ class ProductsService {
       id: faker.datatype.uuid(),
       ...data
     };
-
     this.products.push(newProduct);
 
     return newProduct;
   }
 
   find() {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        resolve(this.products);
-      }, 3000);
-    });
+    return this.products;
   }
 
   async findOne(id) {
-    // const name = this.getLocal();
-    const product = this.products.find((item) => item.id === id);
+    const product = this.products.find(item => item.id === id);
     if (!product) {
       throw boom.notFound('product not found');
     }
-    if (product.isBlocked) {
+    if (product.isBlock) {
       throw boom.conflict('product is block');
     }
 
@@ -73,9 +66,8 @@ class ProductsService {
 
   async delete(id) {
     const index = this.products.findIndex(item => item.id === id);
-
     if (index === -1) {
-      throw new Error('Product not found');
+      throw boom.notFound('product not found');
     }
 
     this.products.splice(index, 1);
