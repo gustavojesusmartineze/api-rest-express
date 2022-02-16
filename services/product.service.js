5300b5e0de474ffadaf56a24954fd8f1d45800b5
Gustavo Martínez <gustavomartinez1990@gmail.com>
2022-02-16 16:51:07 -0400

    feat(product): use pool connection to get data from db

--- services/product.service.js
+++ services/product.service.js
@@ -1,11 +1,14 @@
 const { faker } = require('@faker-js/faker');
 const boom = require('@hapi/boom');
+const { pool } = require('./../libs/postgres-pool')
 
 class ProductsService {
 
   constructor() {
     this.products = [];
     this.generate();
+    this.pool = pool;
+    this.pool.on('error', (err) => console.error(err));
   }
 
   generate() {
@@ -32,8 +35,11 @@ class ProductsService {
     return newProduct;
   }
 
-  find() {
-    return this.products;
+  async find() {
+    const query = 'SELECT * FROM task';
+    const result = await this.pool.query(query);
+
+    return result.rows;
   }
 
   async findOne(id) {
