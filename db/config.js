31cb0e37fc73e648e68cdcfaba7182e4ef8b60af
Gustavo Martínez <gustavomartinez1990@gmail.com>
2022-02-18 21:44:38 -0400

    feat(db): add config for sequelize migrations

---
+++ db/config.js
@@ -0,0 +1,20 @@
+const { config } = require('./../config/config');
+
+const ENGINE = config.db.engine;
+const USER = encodeURIComponent(config.db[ENGINE].user);
+const PASSWORD = encodeURIComponent(config.db[ENGINE].password);
+const HOST = config.db[ENGINE].host;
+const PORT = config.db[ENGINE].port;
+const DATABASE = config.db[ENGINE].database;
+const URI = `${ENGINE}://${USER}:${PASSWORD}@${HOST}:${PORT}/${DATABASE}`;
+
+module.exports = {
+  development: {
+    url: URI,
+    dialect: ENGINE,
+  },
+  production: {
+    url: URI,
+    dialect: ENGINE,
+  }
+}
