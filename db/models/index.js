4cf223e49ada6ef5b1236b008d5347467d7dbd89
Gustavo Martínez <gustavomartinez1990@gmail.com>
2022-02-19 18:55:40 -0400

    feat(db): load asociations for customer model

--- db/models/index.js
+++ db/models/index.js
@@ -4,6 +4,8 @@ const { Customer, CustomerSchema } = require('./customer.model');
 function setupModels(sequelizeConnection) {
   User.init(UserSchema, User.config(sequelizeConnection));
   Customer.init(CustomerSchema, Customer.config(sequelizeConnection));
+
+  Customer.associate(sequelizeConnection.models);
 }
 
 module.exports = setupModels;
