c2d1b30d4cefeb4932688d6bcc197bc9ce7e6293
Gustavo Martínez <gustavomartinez1990@gmail.com>
2022-02-20 19:07:08 -0400

    feat(customer): add userId to Joi schema validation

--- schemas/customer.schema.js
+++ schemas/customer.schema.js
@@ -1,20 +1,23 @@
 const Joi = require('joi');
 
 const id = Joi.number().integer();
-const name = Joi.string().min(3).min(30);
-const lastName = Joi.string().min(3).min(30);
+const name = Joi.string().min(3).max(30);
+const lastName = Joi.string().min(3).max(30);
 const phone = Joi.string();
+const userId = Joi.number().integer();
 
 const createCustomerSchema = Joi.object({
   name: name.required(),
   lastName: lastName.required(),
   phone: phone.required(),
+  userId: userId.required(),
 });
 
 const updateCustomerSchema = Joi.object({
   name: name,
   lastName: lastName,
-  phone: phone
+  phone: phone,
+  userId: userId
 });
 
 const getCustomerSchema = Joi.object({
