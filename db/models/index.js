196a9b479b2e66805e01be9b787542f3e69c09d8
Gustavo Martínez <gustavomartinez1990@gmail.com>
2022-02-21 18:07:49 -0400

    feat(models): add Order to setUpModels

--- db/models/index.js
+++ db/models/index.js
@@ -2,17 +2,20 @@ const { User, UserSchema } = require('./user.model');
 const { Customer, CustomerSchema } = require('./customer.model');
 const { Category, CategorySchema } = require('./category.model');
 const { Product, ProductSchema } = require('./product.model');
+const { Order, OrderSchema } = require('./order.model');
 
 function setupModels(sequelizeConnection) {
   User.init(UserSchema, User.config(sequelizeConnection));
   Customer.init(CustomerSchema, Customer.config(sequelizeConnection));
   Category.init(CategorySchema, Category.config(sequelizeConnection));
   Product.init(ProductSchema, Product.config(sequelizeConnection));
+  Order.init(OrderSchema, Order.config(sequelizeConnection));
 
   User.associate(sequelizeConnection.models);
   Customer.associate(sequelizeConnection.models);
   Category.associate(sequelizeConnection.models);
   Product.associate(sequelizeConnection.models);
+  Order.associate(sequelizeConnection.models);
 }
 
 module.exports = setupModels;
