72c35d59581300ae5f870af7545816fe4b244fd3
Gustavo Martínez <gustavomartinez1990@gmail.com>
2022-02-21 16:18:40 -0400

    feat(models): add Category and Product to setUpModels

--- db/models/index.js
+++ db/models/index.js
@@ -1,12 +1,18 @@
 const { User, UserSchema } = require('./user.model');
 const { Customer, CustomerSchema } = require('./customer.model');
+const { Category, CategorySchema } = require('./category.model');
+const { Product, ProductSchema } = require('./product.model');
 
 function setupModels(sequelizeConnection) {
   User.init(UserSchema, User.config(sequelizeConnection));
   Customer.init(CustomerSchema, Customer.config(sequelizeConnection));
+  Category.init(CategorySchema, Category.config(sequelizeConnection));
+  Product.init(ProductSchema, Product.config(sequelizeConnection));
 
   User.associate(sequelizeConnection.models);
   Customer.associate(sequelizeConnection.models);
+  Category.associate(sequelizeConnection.models);
+  Product.associate(sequelizeConnection.models);
 }
 
 module.exports = setupModels;
