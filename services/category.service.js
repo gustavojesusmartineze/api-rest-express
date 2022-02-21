9b56e09c6123738bd9253cb8d135c607fac0cf57
Gustavo Martínez <gustavomartinez1990@gmail.com>
2022-02-21 18:05:43 -0400

    feat(category): use sequelize models for crud operations

--- services/category.service.js
+++ services/category.service.js
@@ -1,29 +1,44 @@
 const boom = require('@hapi/boom');
+const { models } = require('./../libs/sequelize');
 
 class CategoryService {
 
   constructor(){
   }
+
   async create(data) {
-    return data;
+    const category = await models.Category.create(data);
+
+    return category;
   }
 
   async find() {
-    return [];
+    return await models.Category.findAll();
   }
 
   async findOne(id) {
-    return { id };
+    const cateogry = await models.Category.findByPk(id, {
+      include: ['products']
+    });
+
+    if (!cateogry) {
+      throw boom.notFound('cateogry not found');
+    }
+
+    return cateogry;
   }
 
   async update(id, changes) {
-    return {
-      id,
-      changes,
-    };
+    const category = await this.findOne(id);
+    const updatedCateogry = await category.update(changes);
+
+    return updatedCateogry;
   }
 
   async delete(id) {
+    const category = await this.findOne(id);
+
+    await category.destroy();
     return { id };
   }
 
