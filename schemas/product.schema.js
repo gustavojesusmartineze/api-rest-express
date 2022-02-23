99ae9d8f971783582f2a6f955e2d0a5147e85e4b
Gustavo Martínez <gustavomartinez1990@gmail.com>
2022-02-23 18:37:44 -0400

    feat(products): filter products by price min and max

--- schemas/product.schema.js
+++ schemas/product.schema.js
@@ -7,6 +7,10 @@ const description = Joi.string().min(10);
 const image = Joi.string().uri();
 const categoryId = Joi.number().integer();
 
+
+const price_min = Joi.number().integer().min(10);
+const price_max = Joi.number().integer().min(10);
+
 const limit = Joi.number().integer();
 const offset = Joi.number().integer();
 
@@ -33,6 +37,12 @@ const getProductSchema = Joi.object({
 const queryProductSchema = Joi.object({
   limit: limit,
   offset: offset,
+  price: price,
+  price_min: price_min,
+  price_max: price_max.when('price_min', {
+    is: Joi.number().integer().required(),
+    then: Joi.required()
+  })
 });
 
 module.exports = {
