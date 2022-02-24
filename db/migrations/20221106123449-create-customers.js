1b5c880e903e04da63f5d30d6c191d51923e1f2e
Gustavo Martínez <gustavomartinez1990@gmail.com>
2022-02-23 20:10:41 -0400

    fix(db): set migrations order properly

--- db/migrations/20221106123449-create-customers.js
+++ db/migrations/20221106123449-create-customers.js
@@ -1,11 +1,51 @@
 'use strict';
 
-const { CustomerSchema, CUSTOMER_TABLE } = require('./../models/customer.model');
+const { DataTypes, Sequelize } = require('sequelize');
+const { USER_TABLE } = require('./../models/user.model');
+const { CUSTOMER_TABLE } = require('./../models/customer.model');
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface) {
-    await queryInterface.createTable(CUSTOMER_TABLE, CustomerSchema);
+    await queryInterface.createTable(CUSTOMER_TABLE, {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER
+      },
+      name: {
+        allowNull: false,
+        type: DataTypes.STRING,
+        unique: true,
+      },
+      lastName: {
+        allowNull: true,
+        type: DataTypes.STRING,
+        field: 'last_name',
+      },
+      phone: {
+        allowNull: true,
+        type: DataTypes.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        field: 'create_at',
+        defaultValue: Sequelize.NOW
+      },
+      userId: {
+        field: 'user_id',
+        allowNull: false,
+        type: DataTypes.INTEGER,
+        references: {
+          model: USER_TABLE, // Table name of the reference
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      }
+    });
   },
 
   async down (queryInterface) {
--- db/migrations/20221107000508-order-product.js
+++ db/migrations/20221107000508-order-product.js
@@ -1,11 +1,53 @@
 'use strict';
 
-const { OrderProductSchema, ORDER_PRODUCT_TABLE } = require('./../models/order-product.model');
+const { DataTypes, Sequelize } = require('sequelize');
+const { ORDER_PRODUCT_TABLE } = require('./../models/order-product.model');
+const { ORDER_TABLE } = require('./../models/order.model');
+const { PRODUCT_TABLE } = require('./../models/product.model');
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface) {
-    await queryInterface.createTable(ORDER_PRODUCT_TABLE, OrderProductSchema);
+    await queryInterface.createTable(ORDER_PRODUCT_TABLE, {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        field: 'created_at',
+        defaultValue: Sequelize.NOW,
+      },
+      amount: {
+        allowNull: false,
+        type: DataTypes.INTEGER
+      },
+      orderId: {
+        field: 'order_id',
+        allowNull: false,
+        type: DataTypes.INTEGER,
+        references: {
+          model: ORDER_TABLE,
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
+      productId: {
+        field: 'product_id',
+        allowNull: false,
+        type: DataTypes.INTEGER,
+        references: {
+          model: PRODUCT_TABLE,
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      }
+    });
   },
 
   async down (queryInterface) {
